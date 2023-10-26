c1ab1ab57031d9363a35e6deacdd50db4638e83b
fercholondo <luis.londono4@udea.edu.co>
2023-10-25 23:16:16 -0500

    se crea archivo de ensayo para realizar la prueba con axios

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 async function consumeApiWithFetch(url){
     try{
         const respuesta = await fetch(url);
-        console.log(`la petición a la api se completo correctamente con status: ${respuesta.status}`);
+        console.log(`la petición a la api se completó correctamente con status: ${respuesta.status}`);
         return await respuesta.json();
     } catch(error){
         console.error(`fallo la petición a la api con error: ${error.message}`);
     }    
 }
 
-const resp = consumeApiWithFetch('https://pokeapi.co/api/v2/pokemon/pikachu');
-console.log (resp);
+const resp = consumeApiWithFetch('https://pokeapi.co/api/v2/pokemon/rayquaza');
+
 async function obtenerDatosRespuesta(respuesta){
         const respuestaApi = await respuesta;
-        const datosRespuesta=respuestaApi.data;
-        for (dato of datosRespuesta){
-        console.log(dato);}
+        const datosRespuesta=respuestaApi;
+        console.log('id=', datosRespuesta)
+        return datosRespuesta
  }
-obtenerDatosRespuesta(resp)
+
+const pokemonData = obtenerDatosRespuesta(resp);
+
+
+
+
+
+//ID pokemon
+const idPokemon = pokemonData.id;
+
+//Nombre Pokemon
+const nombre = pokemonData.name;
+
+
+//Habilidades Pokemon
+const habilidades = pokemonData.abilities;
+let habilidadesTexto = "";
+/*for (let i = 0; i < habilidades.length; i++) {
+    if (i > 0) {
+        habilidadesTexto += ", "; 
+    }
+    habilidadesTexto += habilidades[i].ability.name;
+}*/
